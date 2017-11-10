79224b9a22e7d63494d01825113dfa3e47100a0b
WTang06 <33232639+WTang06@users.noreply.github.com>
2017-11-10 14:49:23 -0500

    Stairway to Heaven + bob'sboxes fix

---
+++ StairwayToHeaven/StairwayToHeaven/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StairwayToHeaven
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! I will render a staircase for you.");
+            Console.WriteLine("");
+
+            Console.WriteLine("What size?");
+            Console.ReadLine();
+
+        }
+    }
+}
